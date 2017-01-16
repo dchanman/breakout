e1dd6bfa4fd858aba8877b7e7e60c6d6e438ad77
Derek Chan <derekchan1994@gmail.com>
2017-01-15 23:11:30 -0800

    Added breakout pieces and stacking function

--- public/js/breakout.js
+++ public/js/breakout.js
@@ -1,22 +1,45 @@
 "use strict";
 
 // create board
-var b = jsboard.board({attach: "game", size: "3x3"});
-b.cell("each").style({width: "75px", height: "75px"});
+var b = jsboard.board({
+    attach: "game",
+    size: "8x5",
+    style: "checkerboard"
+});
+
+b.cell("each").style({
+    width: "75px",
+    height: "75px"
+});
 
 // setup pieces
-var piece_x = jsboard.piece({text: "X", fontSize: "45px", textAlign: "center"});
-var piece_o = jsboard.piece({text: "O", fontSize: "45px", textAlign: "center"});
+var SIDE  = {
+    X: "X",
+    O: "O"
+};
+var breakout_piece = function (side, height) {
+    var name = side + height;
+    return jsboard.piece({text: name, fontSize: "35px", textAlign: "center"}).clone();
+};
+var breakout_piece_stack = function (piece, increment) {
+    var name = piece[0] + (parseInt(piece.substring(1), 10) + increment);
+    return jsboard.piece({text: name, fontSize: "35px", textAlign: "center"}).clone();
+};
 
 // alternate turns of x and o
 var turn = true;
 b.cell("each").on("click", function () {
     if (b.cell(this).get() === null) {
         if (turn) {
-            b.cell(this).place(piece_x.clone());
+            b.cell(this).place(breakout_piece(SIDE.X, 1));
         } else {
-            b.cell(this).place(piece_o.clone());
+            b.cell(this).place(breakout_piece(SIDE.O, 1));
         }
         turn = !turn;
+    } else {
+        console.log(b.cell(this).get());
+        var piece = b.cell(this).get(),
+            newPiece = breakout_piece_stack(piece, 1);
+        b.cell(this).place(newPiece);
     }
 });
