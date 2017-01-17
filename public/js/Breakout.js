eae8a1b182bed05fca794d33599e59a2544d923b
Derek Chan <derekchan1994@gmail.com>
2017-01-16 23:39:53 -0800

    Started implementing legalMoves

--- public/js/Breakout.js
+++ public/js/Breakout.js
@@ -29,6 +29,7 @@ Breakout.SIDE = {
  * Create a Breakout piece for a given side, with a given stack height
  * @param {Breakout.SIDE} side
  * @param {Number} height
+ * @returns {jsboard.piece}
  */
 Breakout.piece = function (side, height) {
     var name = side + height;
@@ -36,19 +37,94 @@ Breakout.piece = function (side, height) {
 };
 
 /**
+ * Returns the side which owns a piece.
+ * Assumes a well-formed piece
+ * @param {jsboard.piece} piece
+ * @returns {Breakout.SIDE}
+ */
+Breakout.piece_getSide = function (piece) {
+    return piece[0];
+};
+
+/**
  * Create a Breakout piece as the given piece with 'increment' extra stacks
- * @param {Breakout.piece} piece
+ * @param {jsboard.piece} piece
  * @param {Number} increment
+ * @returns {jsboard.piece}
  */
 Breakout.piece_stack = function (piece, increment) {
     var name = piece[0] + (parseInt(piece.substring(1), 10) + increment);
     return jsboard.piece({text: name, fontSize: "35px", textAlign: "center"}).clone();
 };
 
+Breakout.prototype.resetBoard = function () {
+    var x, y;
+    for (y = 0; y < this.board.cols(); y++) {
+        for (x = 0; x < this.board.rows(); x++) {
+            this.board.cell([x, y]).DOM().classList.remove("legalMove");
+            // this.board.cell([x,y]).removeOn("click", movePiece);
+        }
+    }
+};
+
+/**
+ * Creates a list of all legal moves for a piece
+ * @param {jsboard.piece} piece
+ * @returns {List[[Number, Number]]}
+ */
+Breakout.prototype.getLegalMoves = function (cell) {
+    var piece = this.board.cell(cell).get(),
+        side = Breakout.piece_getSide(piece),
+        loc = this.board.cell(cell).where(),
+        x,
+        y,
+        legalMoves = [],
+        legalMovesHelper;
+    console.log(side);
+    console.log(loc[0] + "," + loc[1]);
+    /**
+     * If x and y is a legal move, add it to legalMoves
+     * @returns {Boolean} true to continue searching, false to break
+     */
+    legalMovesHelper = function (board, x, y) {
+        if (board.cell([y, x]).get() === null) {
+            legalMoves.push([y, x]);
+            return true;
+        }
+        return false;
+    };
+    // Right
+    x = loc[1];
+    for (y = loc[0] + 1; y < this.board.rows(); y++) {
+        if (!legalMovesHelper(this.board, x, y)) { break; }
+    }
+    // Left
+    for (y = loc[0] - 1; y >= 0; y--) {
+        if (!legalMovesHelper(this.board, x, y)) { break; }
+    }
+    // Down
+    y = loc[0];
+    for (x = loc[1] + 1; x < this.board.cols(); x++) {
+        if (!legalMovesHelper(this.board, x, y)) { break; }
+    }
+    // Up
+    for (x = loc[1] - 1; x >= 0; x--) {
+        if (!legalMovesHelper(this.board, x, y)) { break; }
+    }
+    return legalMoves;
+};
+
 Breakout.prototype.onClick = function (cell) {
+    this.resetBoard();
     if (this.board.cell(cell).get() !== null) {
         var piece = this.board.cell(cell).get(),
-            newPiece = Breakout.piece_stack(piece, 1);
+            newPiece = Breakout.piece_stack(piece, 1),
+            legalMoves,
+            i;
         this.board.cell(cell).place(newPiece);
+        legalMoves = this.getLegalMoves(cell);
+        for (i = 0; i < legalMoves.length; i++) {
+            this.board.cell(legalMoves[i]).DOM().classList.add("legalMove");
+        }
     }
 };
